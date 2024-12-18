abd93dd567be789682fc412c5b7d04d0de428034
Niklas <nlm@ractic.dk>
2024-12-18 12:31:18 +0100

    Day 13. Fixed the reload issue in adding to cart.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, session, make_response, url_for, jsonify
+from flask import Flask, render_template, request, redirect, flash, session, make_response, url_for
 from flask_mail import Mail, Message
 from flask_caching import Cache
 import sqlite3
@@ -125,6 +125,7 @@ def cart():
     total_price = sum(float(item['price']) for item in cart_items)
     return render_template('cart.html', cart_items=cart_items, total_price=total_price)
 
+
 @app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
     product_name = request.form.get('product_name')
@@ -134,9 +135,10 @@ def add_to_cart():
         session['cart'] = []
 
     session['cart'].append({'name': product_name, 'price': product_price})
-    flash(f'Added {product_name} to cart.')
+    session.modified = True
 
-    return redirect('/shop')
+    flash(f'{product_name} added to cart!', 'success')
+    return redirect(url_for('shop', _anchor='product-list'))
 
 
 
